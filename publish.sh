0a1503f44032482bbd27fa48af690014459cc9c2
kimchanjung <mogomezwai@gmail.com>
2020-07-29 16:05:57 +0900

    git ignore 추가

--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
+cp .gitignore _site
 git checkout source
 git branch -D master
 git checkout -b master
-cp .gitignore _site
 git filter-branch --subdirectory-filter _site/ -f
 git push --all
 git checkout source
